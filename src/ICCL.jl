0abbeb51dd78b02c5c2f2242acdc4e5291908c72
Tzu-Yao Huang (Asus) <tzuyao.jason.huang@gmail.com>
2026-06-16 22:15:09 +0900

    Move function into different files

---
+++ src/ICCL.jl
@@ -0,0 +1,73 @@
+import WaterLily: loc, inside, δ
+
+function LabelAnalyzeBlob(f::AbstractArray{T,D};blobtarget=0,perdir=(),threshold=T(1e-4)) where {T,D}
+    Ng = size(f)
+    labels = zeros(Int, Ng)
+    current_label = 0
+
+    Blobs = Vector{BlobData{T,D}}()
+    queue = Vector{CartesianIndex{D}}(undef, prod(Ng.-2)÷2)
+
+    isblob(α) = abs(α - blobtarget) < 1 - threshold
+    blobval(α) = abs((1 - blobtarget) - α)
+    
+    domain = inside(f)
+
+    @inbounds for I ∈ domain
+        if isblob(f[I]) && labels[I] == 0
+            current_label += 1
+            labels[I] = current_label
+
+            queue[1] = I
+            head, tail = 1, 1
+
+            vol = zero(T)
+            centroid = zero(SVector{D,T})
+
+            while head <= tail
+                curr = queue[head]
+                head += 1
+
+                val = blobval(f[curr])
+                vol += val
+                centroid += loc(0, curr) * val
+
+                for i in 1:D
+                    for s in (-1, 1)
+                        idx = curr[i] + s
+                        valid = true
+                        
+                        # Handle periodicity using `in`
+                        if idx < 2
+                            if i in perdir
+                                idx = Ng[i] - 1 
+                            else
+                                valid = false   
+                            end
+                        elseif idx >= Ng[i]
+                            if i in perdir
+                                idx = 2         
+                            else
+                                valid = false   
+                            end
+                        end
+                        
+                        !valid && continue
+                        
+                        n = CartesianIndex(ntuple(dim -> dim == i ? idx : curr[dim], D))
+                        
+                        if isblob(f[n]) && labels[n] == 0
+                            labels[n] = current_label
+                            tail += 1
+                            queue[tail] = n
+                        end
+                    end
+                end
+            end
+            
+            push!(Blobs, BlobData(current_label, vol, centroid / vol))
+        end
+    end
+
+    return labels, Blobs
+end
